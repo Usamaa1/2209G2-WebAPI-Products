50e550197e12b34b02cca0f500d86d31fb407426
usamaa1 <usamariz1997@gmail.com>
2024-08-24 20:53:36 +0500

    WebAPI REST Completed

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly ProductsContext _context;
+
+        public ProductsController(ProductsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+
+            return Ok(_context.Products.ToList());
+        }
+        [HttpPost]
+        public IActionResult AddProducts(Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return StatusCode(201);
+            }
+            catch(Exception)
+            {
+                return StatusCode(404);
+            }
+            
+        }
+        [HttpDelete]
+        public IActionResult DeleteProducts(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+    }
+}
---
+++ Data/ProductsContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data;
+
+public partial class ProductsContext : DbContext
+{
+    public ProductsContext()
+    {
+    }
+
+    public ProductsContext(DbContextOptions<ProductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC074DD8A87B");
+
+            entity.Property(e => e.ProdDesc)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("prodDesc");
+            entity.Property(e => e.ProdName)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("prodName");
+            entity.Property(e => e.ProdPrice).HasColumnName("prodPrice");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
